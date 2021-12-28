2f66e026ba2ef0a4953fe8dc2ed67cb1d7f894b0
Alex-Braine <alex.braine.lu@gmail.com>
2021-12-28 16:30:22 +0100

    upd

--- fix_linux.sh
+++ fix_linux.sh
@@ -5,8 +5,10 @@ fix() {
    NEWNAME=$2
    base=$(basename "$FILE")
    newname="\$ORIGIN/../libs/$base"
-   echo "patchelf --replace-needed $FILE $newname $NEWNAME"
-   patchelf --replace-needed "$FILE" "$newname" "$NEWNAME"
+   #echo "patchelf --replace-needed $FILE $newname $NEWNAME"
+   #patchelf --replace-needed "$FILE" "$newname" "$NEWNAME"
+   echo "--set-rpath \$ORIGIN/../libs/ $NEWNAME"
+   patchelf --set-rpath "\$ORIGIN/../libs/" "$NEWNAME"
 }
 
 fixup () {
@@ -16,10 +18,10 @@ fixup () {
     echo "cp $FILE $NEWNAME"
     cp "$FILE" "$NEWNAME"
     LIST=$(patchelf --print-needed $FILE | grep lib)
-    # for FILE in $LIST
-    # do
-    #     fix $FILE $NEWNAME
-    # done
+    for FILE in $LIST
+    do
+        fix $FILE $NEWNAME
+    done
 }
 
 fixup_all () {

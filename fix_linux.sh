9a5c83ac4c7f44296c9a85af28674563343c9580
Alex-Braine <alex.braine.lu@gmail.com>
2021-12-29 20:08:46 +0100

    upd

--- fix_linux.sh
+++ fix_linux.sh
@@ -14,7 +14,7 @@ fixup () {
     NEWNAME="libs/$BASE"
     echo "cp $FILE $NEWNAME"
     cp "$FILE" "$NEWNAME"
-    LIST=$(patchelf --print-needed $FILE | grep lib | grep -v "ld-linux" | grep -v "ld-2")
+    LIST=$(patchelf --print-needed $FILE | grep lib | grep -v "ld-linux" | grep -v "ld-2" | grep -v "libc.so")
     for FILE in $LIST
     do
         fix $FILE $NEWNAME
@@ -28,27 +28,27 @@ fixup_all () {
         ARCH_DIR="x86_64-linux-gnu"
     fi
 
-    LIST=$(patchelf --print-needed src/remote-viewer | grep lib | grep -v "ld-linux" | grep -v "ld-2")
+    LIST=$(patchelf --print-needed src/remote-viewer | grep lib | grep -v "ld-linux" | grep -v "ld-2" | grep -v "libc.so")
     NEWNAME="src/remote-viewer"
     for FILE in $LIST
     do
         fix $FILE $NEWNAME
     done
 
-    mkdir "libs"
-    FILES=$(find "/lib64/" -type f -maxdepth 1 -regex ".*.so.*" | grep -v "ld-linux" | grep -v "ld-2")
-    for f in $FILES
-    do
-        fixup $f
-    done
-
-    FILES=$(find "/lib64/" -type l -maxdepth 1  -regex ".*.so.*" | grep -v "ld-linux" | grep -v "ld-2")
+    FILES=$(find "/lib64/" -type l -regex ".*.so.*" | grep -v "ld-linux" | grep -v "ld-2" | grep -v "libc.so")
     for f in $FILES
     do
         BASE=$(basename "$f")
         NEWNAME="libs/$BASE"
         echo "cp -a $f $NEWNAME"
         cp -a $f $NEWNAME
+    done
+
+    mkdir "libs"
+    FILES=$(find "/lib64/" -type f -regex ".*.so.*" | grep -v "ld-linux" | grep -v "ld-2" | grep -v "libc.so")
+    for f in $FILES
+    do
+        fixup $f
     done
 }
 fixup_all $1

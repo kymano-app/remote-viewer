48e8cb5b647393d474fef549e2f10ceba54f7dca
Alex-Braine <alex.braine.lu@gmail.com>
2021-12-29 22:57:25 +0100

    upd

--- fix_linux.sh
+++ fix_linux.sh
@@ -14,7 +14,7 @@ fixup () {
     NEWNAME="libs/$BASE"
     echo "cp $FILE $NEWNAME"
     cp "$FILE" "$NEWNAME"
-    LIST=$(patchelf --print-needed $FILE | grep lib | grep -v "libcrypto.so.1.1" | grep -v "libssl.so.1.1")
+    LIST=$(patchelf --print-needed $FILE | grep lib)
     for FILE in $LIST
     do
         fix $FILE $NEWNAME
@@ -34,7 +34,7 @@ fixup_all () {
         ARCH_DIR="x86_64-linux-gnu"
     fi
 
-    LIST=$(patchelf --print-needed src/remote-viewer | grep lib | grep -v "libcrypto.so.1.1" | grep -v "libssl.so.1.1")
+    LIST=$(patchelf --print-needed src/remote-viewer | grep lib)
     NEWNAME="src/remote-viewer"
     for FILE in $LIST
     do
@@ -53,7 +53,7 @@ fixup_all () {
     done
 
     mkdir "libs"
-    FILES=$(find "/lib64/" -type l -regex ".*.so.*" | grep -v "libcrypto.so.1.1" | grep -v "libssl.so.1.1")
+    FILES=$(find "/lib64/" -type l -regex ".*.so.*")
     for f in $FILES
     do
         BASE=$(basename "$f")
@@ -62,7 +62,7 @@ fixup_all () {
         cp -a $f $NEWNAME
     done
 
-    FILES=$(find "/lib64/" -type f -regex ".*.so.*" | grep -v "libcrypto.so.1.1" | grep -v "libssl.so.1.1")
+    FILES=$(find "/lib64/" -type f -regex ".*.so.*")
     for FILE in $FILES
     do
         fixup $FILE
